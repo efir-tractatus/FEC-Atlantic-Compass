d11d89999ba779d81e7576782bad05aa34ea5546
David Lai <punkvidi@gmail.com>
2020-09-23 17:17:22 -0400

    added middleware

--- project/server/index.js
+++ project/server/index.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 4206;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../react-client/dist'));
+
+
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
