460304c3ae107147b91bfca232a7199305b265b4
David Lai <punkvidi@gmail.com>
2020-09-23 19:07:36 -0400

    basic call from the api

--- project/server/index.js
+++ project/server/index.js
@@ -1,6 +1,6 @@
+import axios from 'axios';
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 4206;
 
@@ -8,7 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-
+app.get('/catwalk', (req, res) => {
+  return axios.get(`http://18.224.37.110/products/list`)
+    .catch((err) => {
+      console.log(`There was an error: ${err}`)
+    })
+});
 
 
 app.listen(PORT, () => {
