e18cf786e5dbb1763eb637ab1aa8898862b57538
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-28 12:39:06 -0400

    Implement QuestionList component
    Takes in an array of questions as a prop.
    
    Will use map to render a list of QuestionListEntry componenets
    
    Will pass individual question objects to each QuestionListEntry
    
    Renders buttons at bottom of list.  These buttons will eventually allow
    the loading of additional questions or the ability for users to enter
    their own questions via a pop-up modal.
    
    Currently renders 2 questions only.  To be implemented is the button
    which will load additional questions when clicked.

---
+++ project/src/components/QuestionsAndAnswers/QuestionList.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import QuestionListEntry from "./QuestionListEntry";
+
+var QuestionList = (props) => {
+  if (props.questionList.length) {
+    var numOfQuestions = 2
+    var questionsToDisplay= [];
+    for (let i = 0; i < numOfQuestions; i++) {
+      questionsToDisplay.push(props.questionList[i])
+    }
+    return (
+      <div className="question-list">
+        {questionsToDisplay.map((currQuestion) => {
+          return <QuestionListEntry question={currQuestion} key={currQuestion.question_id}/>
+        })
+        }
+        <div className="button-flex-container">
+          <button className="QandA-button-more-questions">MORE ANSWERED QUESTIONS</button>
+          <button className="QandA-button-add-question">ADD A QUESTION +</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {questionsToDisplay.map((currQuestion) => {
+          return <QuestionListEntry question={currQuestion} key={currQuestion.question_id}/>
+        })
+        }
+        <button className="QandA-button-add-question">ADD A QUESTION +</button>
+      </div>
+    );
+  }
+};
+
+QuestionList.propTypes = {};
+
+export default QuestionList;
