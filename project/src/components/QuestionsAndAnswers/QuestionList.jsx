adc00a852585d4cdfebb2d00fd747fbb78bfc928
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-29 15:22:24 -0400

    Implement Add more questions and Add Question Modal
    Numeber of questions entries displayed can now be increased via the MORE
    ANSWERED QUESTIONS button.  Number of questions to be displayed is
    maintained as a useState hook.  Upon clicking the more questions button
    the number will be increased by 2.  A useEffect hook tracking the
    quesiton count state will rerender the question list based on the new
    qty to be rendered.
    
    AddQuestionModal - ADD A QUESTION button will now open a modal for the
    user to enter their own question related to the current product.  This
    is accomplished via a Modal template component and a specific
    AddQuestionModal componenet.  The modal is triggered by a useState hook
    that is toggled via the ADD A QUESTION button.  When toggled to true the
    Modal Template will open the modal via a portal to the "active-modal"
    div within the main html body.

--- project/src/components/QuestionsAndAnswers/QuestionList.jsx
+++ project/src/components/QuestionsAndAnswers/QuestionList.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import QuestionListEntry from "./QuestionListEntry";
+import ModalTemplate from "./ModalTemplate.jsx";
+import AddQuestionModal from "./AddQuestionModal.jsx";
 
 var QuestionList = (props) => {
+  //modal toggle
+  const [isOpen, setIsOpen] = useState(false)
+
   if (props.questionList.length) {
-    var numOfQuestions = 2
-    var questionsToDisplay= [];
-    for (let i = 0; i < numOfQuestions; i++) {
-      questionsToDisplay.push(props.questionList[i])
-    }
-    if (numOfQuestions < props.questionList.length) {
+    var questionList = props.questionList.slice(0);
+    questionList.sort(sortQuestionsByMostHelpful);
+
+    const [numToDisplay, setNumtoDisplay] = useState(2);
+
+    var displayList = [];
+
+   for(let i = 0; i < numToDisplay; i++) {
+     if (questionList[i]) {
+      displayList.push(questionList[i]);
+     }
+   }
+
+    useEffect(() => {
+    setQuestionsToDisplay(displayList);
+    }, [numToDisplay])
+
+    const [questionsToDisplay, setQuestionsToDisplay] = useState(displayList);
+
+    if (numToDisplay < props.questionList.length) {
       return (
         <div className="question-list">
             {questionsToDisplay.map((currQuestion) => {
-              return <QuestionListEntry question={currQuestion} key={currQuestion.question_id}/>
+              return <QuestionListEntry question={currQuestion} key={currQuestion.question_id} productName={props.product.name}/>
             })
             }
           <div className="button-flex-container">
-            <button className="QandA-button-more-questions"><p>MORE ANSWERED QUESTIONS</p></button>
-            <button className="QandA-button-add-question"><p>ADD A QUESTION </p><p className="button-plus">+</p></button>
+            <button className="QandA-button-more-questions" onClick={() => {
+            setNumtoDisplay(numToDisplay + 2)}}><p>MORE ANSWERED QUESTIONS</p></button>
+            <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}><p>ADD A QUESTION </p><p className="button-plus">+</p></button>
           </div>
+          <ModalTemplate open={isOpen} onClose={() => setIsOpen(false)}>
+            <AddQuestionModal productName={props.product.name} productId={props.product.id}/>
+          </ModalTemplate>
         </div>
       );
     } else {
@@ -28,7 +51,10 @@ var QuestionList = (props) => {
             return <QuestionListEntry question={currQuestion} key={currQuestion.question_id}/>
           })
           }
-          <button className="QandA-button-add-question">ADD A QUESTION +</button>
+          <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}>ADD A QUESTION +</button>
+          <ModalTemplate open={isOpen} onClose={() => setIsOpen(false)}>
+          <AddQuestionModal />
+        </ModalTemplate>
         </div>
       );
     }
@@ -36,7 +62,10 @@ var QuestionList = (props) => {
     return (
       <div>
         <p className="question-list-default">NO QUESTIONS YET...</p>
-        <button className="QandA-button-add-question">ADD A QUESTION +</button>
+        <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}>ADD A QUESTION +</button>
+        <ModalTemplate open={isOpen} onClose={() => setIsOpen(false)}>
+          <AddQuestionModal />
+        </ModalTemplate>
       </div>
     )
   }
@@ -45,3 +74,13 @@ var QuestionList = (props) => {
 QuestionList.propTypes = {};
 
 export default QuestionList;
+
+var sortQuestionsByMostHelpful = (question1, question2) => {
+  if (question1.question_helpfulness < question2.question_helpfulness) {
+    return 1;
+  } else if (question1.question_helpfulness > question2.question_helpfulness) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
