defc6636bdff5c9744713e7b55fe7ae1be2aebf9
Denis Sanches <denis.a.sanches@gmail.com>
2020-09-29 17:12:44 -0400

    update product information container to pass currentStyle props

--- project/src/containers/Overview/ProductInformationContainer.js
+++ project/src/containers/Overview/ProductInformationContainer.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 const mapStateToProps = (store) => ({
   primaryProduct: store.primaryProduct,
   primaryProductMetadata: store.primaryProductMetadata,
+  currentStyle: store.currentStyle,
 });
 
 var productInformationContainer = connect(
