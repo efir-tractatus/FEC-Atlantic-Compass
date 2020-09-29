db212a72085372cc945d3c70c94b915004e44033
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-29 15:33:42 -0400

    Use moment.js to format the date

--- project/src/components/QuestionsAndAnswers/AnswerListEntry.jsx
+++ project/src/components/QuestionsAndAnswers/AnswerListEntry.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import moment from 'moment';
 
 var AnswerListEntry = (props) => {
   var { answer } = props
+  var date = answer.date
+  var formattedDate = moment(date).format('MMMM D, YYYY')
   return (
     <div className="answer-list-entry Q-grid-container">
       <p className="answer-label Q-col-1">A:</p>
       <div className="answer-body Q-col-2">{answer.body}</div>
       <div className="answer-utility-container Q-col-2">
         <p className="answer-utility-text">by {answer.answerer_name} ,</p>
-        <p className="answer-utility-date">{answer.date}</p>
+        <p className="answer-utility-date">{formattedDate}</p>
         <p className="answer-utility-break">|</p>
         <p className="answer-utility-text">Helpful?</p>
         <a className="answer-utility-link">Yes</a>
