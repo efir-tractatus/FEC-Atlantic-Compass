1ba9f344e87e5c82fd57c39aab9a34b2654f17ce
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-30 17:49:14 -0400

    Implement helpfullness and report buttons
    Upon click this button will increase the questions helpfullness rating
    by 1.  This will be reflected immediately upon cicking.  This is only a
    superficial display of the helpfullness rating.  Refreshing the page
    will get the official updated rating from the server.  Clicking will
    also invoke a to be written function that will make a post request to
    the API to record the increase in the helpfulness rating on the
    database.
    
    The report button will function in the same way.
    
    Both items will have their display text changed to indicate to the user
    that the button has been clicked.  These buttons can only be clicked
    with effect once.

--- project/src/components/QuestionsAndAnswers/AnswerListEntry.jsx
+++ project/src/components/QuestionsAndAnswers/AnswerListEntry.jsx
@@ -1,22 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from 'moment';
 
 var AnswerListEntry = (props) => {
   var { answer } = props
   var date = answer.date
   var formattedDate = moment(date).format('MMMM D, YYYY')
+  var photoList = buildPhotoList(answer.photos)
 
-  if (answer.photos.length) {
-  var photoList = (
-  <div className="answer-photo-gallery Q-col-2">
-    {answer.photos.map((photo) =>{
-      return <img className="answer-images" src={photo} alt="User Submitted Product Photo" key={photo}></img>
-    })}
-  </div>
-  )
-  } else {
-    var photoList = null
-  }
+  const [helpfulness, setHelpfulness] = useState(answer.helpfulness);
+  const [isMarkedHelpful, setIsMarkedHelpful] = useState(false);
+  const [isReported, setIsReported] = useState(false);
 
   return (
     <div className="answer-list-entry Q-grid-container">
@@ -28,10 +21,25 @@ var AnswerListEntry = (props) => {
         <p className="answer-utility-date">{formattedDate}</p>
         <p className="answer-utility-break">|</p>
         <p className="answer-utility-text">Helpful?</p>
-        <a className="answer-utility-link">Yes</a>
-        <p className="answer-utility-count">({answer.helpfulness})</p>
+        <a className="answer-utility-link" id="answer-helpful" onClick={(e) => {
+            if (!isMarkedHelpful) {
+              setHelpfulness(helpfulness + 1);
+              setIsMarkedHelpful(true);
+              e.target.innerHTML = 'Thanks!';
+              postHelpfulness();
+            }
+          }
+        }>Yes</a>
+        <p className="answer-utility-count">({helpfulness})</p>
         <p className="answer-utility-break">|</p>
-        <a className="answer-utility-link">Report</a>
+        <a className="answer-utility-link" onClick={(e) => {
+            if (!isReported) {
+              setIsReported(true);
+              e.target.innerHTML = 'Reported!';
+              postReported();
+            }
+          }
+        }>Report</a>
       </div>
     </div>
   );
@@ -40,3 +48,25 @@ var AnswerListEntry = (props) => {
 AnswerListEntry.propTypes = {};
 
 export default AnswerListEntry;
+
+var buildPhotoList = (photoArray) => {
+  if (photoArray.length) {
+    return (
+    <div className="answer-photo-gallery Q-col-2">
+      {photoArray.map((photo) =>{
+        return <img className="answer-images" src={photo} alt="User Submitted Product Photo" key={photo}></img>
+      })}
+    </div>
+    )
+  } else {
+    return null
+  }
+}
+
+var postHelpfulness = () => {
+
+}
+
+var postReported = () => {
+
+}
