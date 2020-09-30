211968a39e91c273c11ed0356465841544bd2b8c
Denis Sanches <denis.a.sanches@gmail.com>
2020-09-30 17:05:10 -0400

    add size selector to Add to cart component

--- project/src/components/Overview/AddToCart.jsx
+++ project/src/components/Overview/AddToCart.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import SelectSize from './SelectSize.jsx';
 
 const AddToCart = (props) => {
   console.log('AddToCart', props);
-  return <h2>AddToCart Section</h2>;
+  var skus = [];
+  var skusObject = {};
+
+  for (var i in props.currentStyle.skus) {
+    if (skusObject.hasOwnProperty(props.currentStyle.skus[i].size)) {
+      skusObject[props.currentStyle.skus[i].size] +=
+        props.currentStyle.skus[i].quantity;
+    } else {
+      skusObject[props.currentStyle.skus[i].size] =
+        props.currentStyle.skus[i].quantity;
+    }
+  }
+  skus = Object.entries(skusObject);
+
+  var renderSkus = skus.map((sku) => {
+    return <SelectSize sku={sku} key={sku[0]} />;
+  });
+
+  return (
+    <div>
+      <h2>AddToCart Section</h2>
+      <select className="size-selector">{renderSkus}</select>
+    </div>
+  );
 };
 
 export default AddToCart;
---
+++ project/src/components/Overview/SelectSize.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SelectSize = ({ sku }) => {
+  return <option>{sku[0]}</option>;
+};
+
+export default SelectSize;
