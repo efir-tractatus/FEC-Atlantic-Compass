e43c363f24eac1bef08d19b819bcb65b4b19d935
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-30 15:00:41 -0400

    Add rendering of user submitted photos if they exist

--- project/src/components/QuestionsAndAnswers/AnswerListEntry.jsx
+++ project/src/components/QuestionsAndAnswers/AnswerListEntry.jsx
@@ -5,10 +5,24 @@ var AnswerListEntry = (props) => {
   var { answer } = props
   var date = answer.date
   var formattedDate = moment(date).format('MMMM D, YYYY')
+
+  if (answer.photos.length) {
+  var photoList = (
+  <div className="answer-photo-gallery Q-col-2">
+    {answer.photos.map((photo) =>{
+      return <img className="answer-images" src={photo} alt="User Submitted Product Photo"></img>
+    })}
+  </div>
+  )
+  } else {
+    var photoList = null
+  }
+
   return (
     <div className="answer-list-entry Q-grid-container">
       <p className="answer-label Q-col-1">A:</p>
       <div className="answer-body Q-col-2">{answer.body}</div>
+      {photoList}
       <div className="answer-utility-container Q-col-2">
         <p className="answer-utility-text">by {answer.answerer_name} ,</p>
         <p className="answer-utility-date">{formattedDate}</p>
