6684bce6c9100559a6087a9a1b46f01460688542
Joe Dimartino <j.dimartino11@gmail.com>
2020-09-30 14:25:17 -0400

    Refactor list filtering and sorting based on search term.
    Debugged and streamlined process for live filtering of the question list
    based on the serach term passed down from the parent component.
    
    Upon a change in the searchInput or the numToDisplay state a useEffect hook will - copy the full
    list of questions for the product - filter it based on the search term
    if the search term in 3 chars or longer - sort it based on helpfullness
    and display the number of questions specified by the current numToDisplay state.
    
    It will also track the currentyl available questions based on the
    current search filter so that the component will know whether or not to
    make the LOAD MORE QUESTIONS button available.
    
    This commit also eliminates the nesting of conditional return statement
    for the render and instead uses a sequential chain of if statements.

--- project/src/components/QuestionsAndAnswers/QuestionList.jsx
+++ project/src/components/QuestionsAndAnswers/QuestionList.jsx
@@ -4,74 +4,87 @@ import ModalTemplate from "./ModalTemplate.jsx";
 import AddQuestionModal from "./AddQuestionModal.jsx";
 
 var QuestionList = (props) => {
-  //modal toggle
+  console.log('Question list recieved:', props.questionList)
+  console.log('Search input recieved:', props.searchInput)
+  var searchInput = props.searchInput
+
   const [isOpen, setIsOpen] = useState(false)
+  const [numToDisplay, setNumtoDisplay] = useState(2);
+  const [isLoading, setIsLoading] = useState(false);
+  const [availableQuestions, setAvailableQuestions] = useState(props.questionList);
+  const [questionsToDisplay, setQuestionsToDisplay] = useState(props.questionList);
 
-  if (props.questionList.length) {
+  useEffect(() => {
+    setIsLoading(true)
     var questionList = props.questionList.slice(0);
-    questionList.sort(sortQuestionsByMostHelpful);
 
-    const [numToDisplay, setNumtoDisplay] = useState(2);
+    if (searchInput.length >= 3) {
+      var processedList = questionList.filter((question) => {
+        return question.question_body.toLowerCase().includes(searchInput.toLowerCase());
+      })
+    } else {
+      var processedList = questionList;
+    }
+
+    if (processedList.length > 1) {
+      processedList = processedList.sort(sortQuestionsByMostHelpful);
+    }
 
     var displayList = [];
 
-   for(let i = 0; i < numToDisplay; i++) {
-     if (questionList[i]) {
-      displayList.push(questionList[i]);
-     }
-   }
-
-    useEffect(() => {
-    setQuestionsToDisplay(displayList);
-    }, [numToDisplay])
-
-    const [questionsToDisplay, setQuestionsToDisplay] = useState(displayList);
-
-    if (numToDisplay < props.questionList.length) {
-      return (
-        <div className="question-list">
-            {buildQuestionList(questionsToDisplay, props)}
-          <div className="button-flex-container">
-            <button className="QandA-button-more-questions" onClick={() => {
-            setNumtoDisplay(numToDisplay + 2)}}><p>MORE ANSWERED QUESTIONS</p></button>
-            <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}><p>ADD A QUESTION </p><p className="button-plus">+</p></button>
-          </div>
-          {buildAddQuestionModal(isOpen, setIsOpen, props)}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {buildQuestionList(questionsToDisplay, props)}
-          <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}>ADD A QUESTION +</button>
-          {buildAddQuestionModal(isOpen, setIsOpen, props)}
-        </div>
-      );
+    for (let i = 0; i < numToDisplay; i++) {
+      if (processedList[i]) {
+        displayList.push(processedList[i]);
+      }
     }
+
+    setAvailableQuestions(processedList);
+    setQuestionsToDisplay(displayList);
+
+    setIsLoading(false);
+  }, [searchInput, numToDisplay])
+
+  if (isLoading === true) {
+    return (
+    <div className="question-list-default">LOADING QUESTIONS...</div>
+    )
+  }
+
+  if (availableQuestions.length <= 0) {
+    return (
+    <div>
+      <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}>ADD A QUESTION +</button>
+      {buildAddQuestionModal(isOpen, setIsOpen, props)}
+    </div>
+    )
+  }
+
+  if (numToDisplay < availableQuestions.length) {
+    return (
+      <div className="question-list">
+          {buildQuestionList(questionsToDisplay, props)}
+        <div className="button-flex-container">
+          <button className="QandA-button-more-questions" onClick={() => {
+          setNumtoDisplay(numToDisplay + 2)}}><p>MORE ANSWERED QUESTIONS</p></button>
+          <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}><p>ADD A QUESTION </p><p className="button-plus">+</p></button>
+        </div>
+        {buildAddQuestionModal(isOpen, setIsOpen, props)}
+      </div>
+    );
   } else {
     return (
       <div>
-        <p className="question-list-default">NO QUESTIONS YET...</p>
+        {buildQuestionList(questionsToDisplay, props)}
         <button className="QandA-button-add-question" onClick={() => setIsOpen(true)}>ADD A QUESTION +</button>
         {buildAddQuestionModal(isOpen, setIsOpen, props)}
       </div>
-    )
+    );
   }
 };
 
 QuestionList.propTypes = {};
 
 export default QuestionList;
-
-var sortQuestionsByMostHelpful = (question1, question2) => {
-  if (question1.question_helpfulness < question2.question_helpfulness) {
-    return 1;
-  } else if (question1.question_helpfulness > question2.question_helpfulness) {
-    return -1;
-  } else {
-    return 0;
-  }
-}
 
 var buildQuestionList = (listOfQuestions, propList) => {
   return listOfQuestions.map((currQuestion) => {
@@ -86,4 +99,14 @@ var buildAddQuestionModal = (isOpen, setIsOpen, propList) => {
     </ModalTemplate>
   )
 
+}
+
+var sortQuestionsByMostHelpful = (question1, question2) => {
+  if (question1.question_helpfulness < question2.question_helpfulness) {
+    return 1;
+  } else if (question1.question_helpfulness > question2.question_helpfulness) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
