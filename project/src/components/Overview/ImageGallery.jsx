c08d04c21c44a471b311eaa84992a04d85f619e2
Denis Sanches <denis.a.sanches@gmail.com>
2020-10-01 14:22:47 -0400

    refactor ImageGallery component to use currentStyle store data instead of currentImage

--- project/src/components/Overview/ImageGallery.jsx
+++ project/src/components/Overview/ImageGallery.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageGalleryItem from './ImageGalleryItem.jsx';
 
 const ImageGallery = (props) => {
   console.log('ImageGallery', props);
+  
+  const [image, setImage] = useState(props.currentStyle.photos[0].url);
 
-  var renderImages = props.currentImages.map((image, index) => {
-    return <ImageGalleryItem image={image} key={index} />;
+  var renderImages = props.currentStyle.photos.map((image, index) => {
+    return (
+      <div
+        className="image-gallery-item"
+        key={index}
+        onClick={() => {
+          setImage(image.url)
+        }}
+      >
+        <img className="image-gallery-thumbnail" src={image.thumbnail_url} />
+      </div>
+    );
   });
 
   return (
     <div>
       <h2>ImageGallery Section</h2>
       <div>
-        <img src={props.currentImage.url} />
+        <img src={image} />
         {renderImages}
       </div>
     </div>
