ffe5eb727befa29c84a6098da9310da4a6545764
David Lai <punkvidi@gmail.com>
2020-10-03 18:43:56 -0400

    weird bug

--- project/src/components/Overview/StyleSelector.jsx
+++ project/src/components/Overview/StyleSelector.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import StyleSelectorItem from './StyleSelectorItem.jsx';
 
 const StyleSelector = (props) => {
-<<<<<<< HEAD
- //   console.log('StyleSelector', props)
-    return <h2>StyleSelector Section</h2>
-}
-=======
   // console.log('StyleSelector', props);
->>>>>>> ed590664081c269cabfd1b7441d5239ac01767cd
 
   var styles = props.currentStyles.map((style) => {
     return <StyleSelectorItem style={style} handleStyleChange={props.handleStyleChange} key={style.style_id} />;
