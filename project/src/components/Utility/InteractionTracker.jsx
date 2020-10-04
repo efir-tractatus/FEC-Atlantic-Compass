76bda41ec53279dd8211494d87c02d5c807bbc38
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-04 17:43:32 -0400

    Create InteractionTrakcer component
    Creates a compnent that will track user interactions on the page.
    Interaction tracker will wrap around the item to be interacted with.
    The item to be interacted with is passed into the Interactions tracker
    as its render prop.  BY wrapping the item to be interacted with in a
    function you can provide the item with the postInteraction() method from
    the Interaction tracker.  Using this method will set up a post request
    to the interaction tracking api.  It will expect an element and widget
    prop to also be set for the instance of the Interaction tracker.  The
    widget prop will indicate which widget of the site is being interacted
    with.  The element prop will indicate which specific element of the
    widget is being interacted with.

---
+++ project/src/components/Utility/InteractionTracker.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+
+class InteractionTracker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.postInteraction = this.postInteraction.bind(this);
+  }
+
+  postInteraction() {
+    axios.post('http://18.224.37.110/interactions', {
+      element: this.props.element,
+      widget: this.props.widget,
+      time: new Date()
+    })
+    .then((response) => {
+      console.log('success posting interaction', response);
+    })
+    .catch((error) => {
+      console.log('error posting interaction', error);
+    })
+  }
+
+  render() {
+    return this.props.render({
+      postInteraction: this.postInteraction
+   })
+  }
+}
+
+export default InteractionTracker
