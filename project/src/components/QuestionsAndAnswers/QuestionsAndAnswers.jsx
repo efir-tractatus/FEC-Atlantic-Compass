3a23c343b9f38720d3a8fa209a09af95f6302898
Denis Sanches <denis.a.sanches@gmail.com>
2020-10-04 20:02:09 -0400

    remove class name

--- project/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ project/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -3,7 +3,7 @@ import React from "react";
 var QuestionsAndAnswers = (props) => {
   return (
     <h1>
-      <div id="question-and-answers">Questions And Answers Component</div>
+      <div>Questions And Answers Component</div>
       <div>{props.productQuestions[0].question_body}</div>
     </h1>
   );
