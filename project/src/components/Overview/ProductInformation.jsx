a4f16c3c489f72690c45e0ff9a798e907b4a4ff5
Denis Sanches <denis.a.sanches@gmail.com>
2020-10-05 14:22:36 -0400

    pass productMetadata to ProductInformation component

--- project/src/components/Overview/ProductInformation.jsx
+++ project/src/components/Overview/ProductInformation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProductInformation = (props) => {
-//   console.log('Product Information', props);
+  console.log('Product Information', props);
 
   if (props.currentStyle.sale_price === '0') {
     var price = <p>{props.currentStyle.original_price}</p>;
@@ -20,7 +20,7 @@ const ProductInformation = (props) => {
     <div className="product-information">
       <h2>ProductInformation Section</h2>
       <h3>Star Rating Placeholder</h3>
-      <h4><a href='#question-and-answers'>Read all {props.reviews.length} Reviews</a></h4>
+      <h4><a href='#topLevelRatingsAndReviews'>Read all {props.reviews.length} Reviews</a></h4>
       <p className='product-category'>{props.primaryProduct.category}</p>
       <p className='product-name'>{props.primaryProduct.name}</p>
       {price}
