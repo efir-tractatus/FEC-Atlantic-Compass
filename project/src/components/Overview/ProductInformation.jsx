2ffbb350bb95fbc4cd6e9b4cd109adb01de1eb53
David Lai <punkvidi@gmail.com>
2020-10-07 15:46:25 -0400

    Update ProductInformation.jsx

--- project/src/components/Overview/ProductInformation.jsx
+++ project/src/components/Overview/ProductInformation.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const ProductInformation = (props) => {
-//  console.log('Product Information', props);
+  console.log('Product Information', props);
 
   if (props.currentStyle.sale_price === '0') {
     var price = <p>{props.currentStyle.original_price}</p>;
   } else {
     var price = (
       <div>
-        <p className="sale-price">{props.currentStyle.sale_price}</p>
-        <p>
+        <p className="sale-price">
+          {props.currentStyle.sale_price}{' '}
           <strike>{props.currentStyle.original_price}</strike>
         </p>
       </div>
@@ -17,13 +17,15 @@ const ProductInformation = (props) => {
   }
 
   return (
-    <div className="product-information">
+    <div className="product-information-box">
       <h2>ProductInformation Section</h2>
       <h3>Star Rating Placeholder</h3>
+      <h4><a href='#topLevelRatingsAndReviews'>Read all {props.reviews.length} Reviews</a></h4>
       <p className='product-category'>{props.primaryProduct.category}</p>
-      <p className='product-name'>{props.currentStyle.name}</p>
+      <p className='product-name'>{props.primaryProduct.name}</p>
       {price}
-      <p className='product-description'>{props.primaryProduct.description}</p>
+      <p className="product-description">{props.primaryProduct.description}</p>
+      <p>Share On Social Media Placeholder</p>
     </div>
   );
 };
